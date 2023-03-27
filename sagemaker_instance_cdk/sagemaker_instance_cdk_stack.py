bd5f425584b1d2ce045a0b676c9276fed41eddba
Rajat Goel <rajatgoel@Rajats-MacBook-Pro.local>
2023-03-26 22:53:22 -0700

    Create sagemaker notebook instance using CDK

--- sagemaker_instance_cdk/sagemaker_instance_cdk_stack.py
+++ sagemaker_instance_cdk/sagemaker_instance_cdk_stack.py
@@ -3,24 +3,27 @@ from aws_cdk import (
     Duration,
     Stack,
     aws_iam as iam,
-    aws_sqs as sqs,
-    aws_sns as sns,
-    aws_sns_subscriptions as subs,
+    aws_sagemaker as sagemaker,
 )
 
+from sagemaker_instance_cdk.props_lib import get_sagemaker_notebook_instance_creation_role
 
 class SagemakerInstanceCdkStack(Stack):
 
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        queue = sqs.Queue(
-            self, "SagemakerInstanceCdkQueue",
-            visibility_timeout=Duration.seconds(300),
-        )
+        # sagemakerInstanceCreationRole = iam.CfnRole(
+        #     self, "NinjaTechAiAssessmentSagemakerExecutionRole",
+        #     assume_role_policy_document=get_sagemaker_notebook_instance_creation_role()
+        #     # managed_policy_arns=[
+        #     #     "arn:aws:iam::aws:policy/AmazonSageMakerFullAccess"
+        #     # ]
+        # )
 
-        topic = sns.Topic(
-            self, "SagemakerInstanceCdkTopic"
+        sagemakerNotebookInstance = sagemaker.CfnNotebookInstance(
+            self, "SagemakeInstanceCdkNotebook",
+            # instance_type='ml.g5.xlarge', # single GPU 16GB memory
+            instance_type='ml.t2.medium',
+            role_arn="arn:aws:iam::340019311433:role/AWSSagemakerExecutionRole"
         )
-
-        topic.add_subscription(subs.SqsSubscription(queue))
